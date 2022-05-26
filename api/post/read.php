050274d2cdea4d05b8721c304106934201c37427
Kadhirravan R <rsktrk@gmail.com>
2022-05-26 15:59:53 +0530

    Added Basic Create post and read one post and all posts

---
+++ api/post/read.php
@@ -0,0 +1,48 @@
+<?php
+    //Headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
+    include_once '../../config/Database.php';
+    include_once '../../models/Post.php';
+
+    $database = new Database();
+    $db = $database->connect();
+
+    // Instantiate blog post object
+    $post = new Post($db);
+    
+    //Blog posts query
+    $result = $post->read();
+
+    $num = $result->rowCount();
+
+    if($num > 0){
+        $posts_arr = array();
+        $posts_arr['data'] = array();
+        
+        while($row = $result->fetch(PDO::FETCH_ASSOC)){
+            extract($row);
+
+            $post_item = array(
+                'id' => $id,
+                'title' => $title,
+                'body' => html_entity_decode($body),
+                'author' => $author,
+                'category_id' => $category_id,
+                'category_name' => $category_name
+            );
+
+            // Push to data
+            array_push($posts_arr['data'], $post_item);
+        }
+
+        // convert to JSON
+        echo json_encode($posts_arr);
+    }else {
+        echo json_encode(
+            array('message' => 'No posts')
+        );
+    }
+
+?>
